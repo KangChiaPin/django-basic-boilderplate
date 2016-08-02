968c9d9c7a8f9a955305d86398dcd3897f18450f
KangChiaPin <billy5521@gmail.com>
2016-08-02 14:07:18 +0800

    add livereload

--- web/settings.py
+++ web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
     'webpack_loader',
     'example',
     ]
@@ -55,6 +56,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
     ]
 
 ROOT_URLCONF = 'web.urls'
